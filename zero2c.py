0c2fb6552d3044f3e5e5e9d92812d49e13b55019
Anima Omnium <119851531+animaomnium@users.noreply.github.com>
2022-12-10 09:01:03 -0300

    Update zero2c.py

--- zero2c.py
+++ zero2c.py
@@ -96,8 +96,6 @@ RUNTIME = """
 
 print(RUNTIME)
 
-
-"""
 def generate_param(n):
   print("typedef struct {")
   for i in range(n):
@@ -106,11 +104,10 @@ def generate_param(n):
 
 for i in range(0, 5):
   generate_param(i)
-"""
 
 def declare_input_registers(n, regs):
   for i in range(n):
-    print(f"  uint64_t r{i} = a[i];")
+    print(f"  uint64_t r{i} = a.v{i};")
     regs.add(i)
 
 def emit_label(i, iden, fun):
@@ -122,9 +119,9 @@ def wrap_t(vals):
   n = len(vals)
   nvals = []
   for i, val in enumerate(vals):
-    nvals.append(f"{val}")
+    nvals.append(f"v{i}: {val}")
   inner = ", ".join(nvals)
-  return f"[{inner}]"
+  return f"struct {{ {inner} }}"
 
 def op2(op):
   def f(a, b):
@@ -256,13 +253,13 @@ def emit_line(i, line, fun, regs, out, funs):
     error(i, line)
 
   for n, result in enumerate(results):
-    print(f"  r{result} = o{i}[{n}];")
+    print(f"  r{result} = o{i}.v{n};")
 
 for fun, (inr, out, block) in funs.items():
   regs = set()
   print()
-  print(f"uint64_t[{out}] ", end="")
-  print(f"{fun}(uint64_t[{inr}] a) {{")
+  print(f"param{out}_t ", end="")
+  print(f"{fun}(param{inr}_t a) {{")
   declare_input_registers(inr, regs)
   
   for i, line in block:
@@ -274,7 +271,7 @@ assert "start" in funs
 
 ENTRYPOINT = """
 int main() {
-  start({0});
+  start(struct {0});
   return 0;
 }
 """
