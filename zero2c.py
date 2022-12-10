1db60320efa4532dd6076d94e3569284e5b6cb5e
Anima Omnium <119851531+animaomnium@users.noreply.github.com>
2022-12-10 09:13:27 -0300

    Update zero2c.py

--- zero2c.py
+++ zero2c.py
@@ -133,7 +133,7 @@ def ret(*args):
 
 def jump2(op):
   def f(label, a, b):
-    return f"{a} {op} {b} ? (goto {label}) : { wrap_t(['0LL']) }"
+    return f"if ({a} {op} {b}) { goto {label}); }"
   return f
 
 OP_simple_case = {
